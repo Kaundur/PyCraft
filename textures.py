86c61266d893c06459e72b30f9567fd530a5ebed
Kaundur <gav.j.jones@googlemail.com>
2016-05-10 13:46:10 +0100

    code tidy

--- textures.py
+++ textures.py
@@ -12,9 +12,6 @@ class Textures:
 
         # Load in textures
         self.texture_main = TextureGroup(pyglet.image.load(self.texture_path).get_texture())
-        self.menu_textures = TextureGroup(pyglet.image.load(self.texture_path).get_texture())
-
-
 
         self.textures = {}
         self.load_texture_dictionary()
@@ -44,26 +41,14 @@ class Textures:
 
         return voxel_texture
 
-
     def _get_texture(self, top, bottom, sides):
         texture_coords = []
-
         texture_coords.append(self.texture_face_coord(top[0], top[1]))
         texture_coords.append(self.texture_face_coord(bottom[0], bottom[1]))
         texture_coords.append(self.texture_face_coord(sides[0], sides[1]))
         texture_coords.append(self.texture_face_coord(sides[0], sides[1]))
         texture_coords.append(self.texture_face_coord(sides[0], sides[1]))
         texture_coords.append(self.texture_face_coord(sides[0], sides[1]))
-
-
-        # This is the whole block, only require the face
-        # texture_coords.extend(self.texture_face_coord(top[0], top[1]))
-        # texture_coords.extend(self.texture_face_coord(bottom[0], bottom[1]))
-        #
-        # texture_coords.extend(self.texture_face_coord(sides[0], sides[1]))
-        # texture_coords.extend(self.texture_face_coord(sides[0], sides[1]))
-        # texture_coords.extend(self.texture_face_coord(sides[0], sides[1]))
-        # texture_coords.extend(self.texture_face_coord(sides[0], sides[1]))
 
         return texture_coords
 
@@ -77,14 +62,3 @@ class Textures:
         # TODO - Is it better to reverse the order of the texture, or should the voxel be reversed ?
         # return x, y, x+dx, y, x+dx, y+dy, x, y+dy
         return x+dx, y+dy, x, y+dy, x, y, x+dx, y
-
-# TODO - REMOVE Menu_items graphics
-def menu_items():
-
-    texture_path = 'Images/GameMenu.png'
-
-    #texture_main = TextureGroup(pyglet.image.load(texture_path).get_texture())
-
-    texture_main = None
-
-    return texture_main
